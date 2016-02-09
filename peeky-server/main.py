3c9c797fde24f46831981ef0bee45916d5f92e03
Gustavo Puma <vash47@gmail.com>
2016-02-09 18:59:04 +1000

    it takes a picture and uploads it to the ftp server

--- peeky-server/main.py
+++ peeky-server/main.py
@@ -1,7 +1,12 @@
+# written by Gustavo Hernando Puma Tejada
+
 import cv2
 import pytz
+import ntpath
 from datetime import datetime
+
 from tzlocal import get_localzone
+from ftplib import FTP
 
 
 # gets the UTC offset in hours
@@ -22,11 +27,31 @@ def get_time_str():
 
 
 # saves the specified frame to disk
+# returns the full path to the file
 def take_snapshot(f):
-    # todo: check. this is currently not being used
-    y, x, z = frame.shape
-    cv2.imwrite(img_path + "caca" + ".jpg", f, img_params)
+    full_img_path = img_path + "caca" + ".jpg"
+    cv2.imwrite(full_img_path, f, img_params)
     print 'img has been saved'
+    return full_img_path
+
+
+# reads the ftp details from a text file
+# text file should have three lines:
+# <host>
+# <user>
+# <password>
+def get_ftp_credentials(filename):
+    with open(filename) as f:
+        content = f.readlines()
+    return [x.strip('\n') for x in content]
+
+
+def upload(ftp, full_path):
+    # we only specify filename on the server; no path
+    fn_only = ntpath.split(full_path)[1]
+    # todo: check block size
+    print "STOR " + fn_only
+    ftp.storbinary("STOR " + fn_only, open(full_path, "rb"))
 
 img_path = "D:\\"
 img_params = [cv2.IMWRITE_JPEG_QUALITY, 60]
@@ -36,24 +61,33 @@ org_pos2 = (0, 60)
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
-if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
-else:
-    rval = False
+rval = False
 
-while rval:
-    cv2.imshow("preview", frame)
-    rval, frame = vc.read()
-    utc_time, local_time = get_time_str()
-    cv2.putText(frame, local_time, org_pos, cv2.FONT_HERSHEY_DUPLEX, .8, (255,255,255), 1, lineType=8)
-    cv2.putText(frame, utc_time, org_pos2, cv2.FONT_HERSHEY_DUPLEX, .8, (255,255,255), 1, lineType=8)
-    key = cv2.waitKey(20)
-    # exit on ESC
-    if key == 27:
-        break
-    # if ENTER key
-    if key == 13:
-        take_snapshot(frame)
+# we loop until we get a frame
+while not rval:
+    # try to get the first frame
+    if vc.isOpened():
+        rval, frame = vc.read()
+    else:
+        rval = False
+utc_time, local_time = get_time_str()
+cv2.putText(frame, local_time, org_pos, cv2.FONT_HERSHEY_DUPLEX, .8, (255,255,255), 1, lineType=8)
+cv2.putText(frame, utc_time, org_pos2, cv2.FONT_HERSHEY_DUPLEX, .8, (255,255,255), 1, lineType=8)
+img_path = take_snapshot(frame)
 cv2.destroyWindow("preview")
 
-
+# FTP UPLOAD
+host, user, pwd = get_ftp_credentials("ftp.txt")
+try:
+    ftp = FTP(host)
+    print 'connecting to host'
+    ftp.login(user, pwd)
+    print 'success. uploading image now...'
+    ftp.cwd("public_html/")
+    upload(ftp, img_path)
+    print 'img upload was successful. disconnecting...'
+except:
+    print 'problem with FTP connection'
+    raise
+finally:
+    ftp.quit()
