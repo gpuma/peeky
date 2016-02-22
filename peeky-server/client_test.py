628bcd419f1751cfd6cc1de76bf6080a862bcd8f
Gustavo Puma <vash47@gmail.com>
2016-02-22 14:43:57 +1000

    removed unused files

--- peeky-server/client_test.py
+++
@@ -1,7 +0,0 @@
-from mysocket import MySocket
-
-print 'creating client socket'
-sock = MySocket()
-print 'attempting to connect to server'
-sock.connect('localhost', 6666)
-print 'connection ended'
